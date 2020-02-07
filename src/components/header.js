71da541203235ac4319efed43e9fabcc39266534
Dolly Chang <dollychang@qnap.com>
2020-02-08 00:45:38 +0800

    add header imgs to png

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import TitleImage from "../assets/header01.svg"
-import DetectiveImage from "../assets/header02.svg"
+// import TitleImage from "../assets/header01.png"
+// import DetectiveImage from "../assets/header02.png"
 import Icon from "../assets/navLogoIcon.svg"
 import More from "../assets/navMoreIcon.svg"
 
@@ -25,14 +25,16 @@ const Header = ({ siteTitle }) => (
     <div className="container keyview">
       <div className="kv-content" >
         <div className="title">
-          <TitleImage />
+          <img src="/assets/header01.png"></img>
+          {/* <TitleImage /> */}
           <h1>
             <span>你今天</span><br />
             <span className="sec-title">買到口罩了嗎？</span>
           </h1>
         </div>
         <div className="img-content" >
-          <DetectiveImage />
+          <img src="/assets/header02.png"></img>
+          {/* <DetectiveImage /> */}
         </div>
       </div>
     </div>
